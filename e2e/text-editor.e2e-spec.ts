4ef03b7d10fbc70eca9d74e625a2a2e68cc85cc4
Alex <alex.kingman@blackbaud.com>
2021-06-18 15:53:21 -0400

    initial commit (#2)
    * initial commit
    
    * removed unncecessary components for initial launch. using skyux toolbars
    
    * disable tests
    
    * moved dompurify to peer dependency
    
    * tightened up rich-text-display component
    
    * changed text input to richText
    
    * removed components from public API; default values using upper case notation
    
    * Documentation updates
    
    * tidied up toolbars; added default values for docs
    
    * toolbar spacing and design tweaks
    
    * finished code coverage
    
    * visual tests
    
    * PR feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>
    
    * removed old locale strings
    
    * SkyTextEditorAdapterSerivce for DOM management
    
    * bumped dependencies
    
    * fixed font picker bug; removed unnecessary visual commit
    
    * added IE11 affordances in unit tests
    
    * accidental commit
    
    * added resource strings for menubar
    
    * more IE11 affordances in tests
    
    * added xs breakpoints to e2e tests
    
    * docs update
    
    * removed static methods; added return types
    
    * renamed menubarActions to menus
    
    * words
    
    * fixed test destroy
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: John Lyons <John.Lyons@blackbaud.com>

---
+++ e2e/text-editor.e2e-spec.ts
@@ -0,0 +1,133 @@
+import {
+  SkyHostBrowserBreakpoint
+} from '@skyux-sdk/e2e/host-browser/host-browser-breakpoint';
+
+import {
+  expect,
+  SkyHostBrowser,
+  SkyVisualThemeSelector
+} from '@skyux-sdk/e2e';
+
+import {
+  by,
+  element
+} from 'protractor';
+
+describe('Text editor', () => {
+  let browserSize: SkyHostBrowserBreakpoint;
+  let currentTheme: string;
+  let currentThemeMode: string;
+
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  async function setBrowserSize(size: SkyHostBrowserBreakpoint): Promise<void> {
+    browserSize = size;
+
+    return SkyHostBrowser.setWindowBreakpoint(size);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (browserSize) {
+      name += '-' + browserSize;
+    }
+
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function runTests(): void {
+    it('should match screenshot', (done) => {
+      expect('#screenshot-text-editor').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('text-editor')
+      });
+    });
+
+    it('should match link modal screenshot', async (done) => {
+      await element.all(by.css('.sky-text-editor-toolbar-action-Link button')).first().click();
+      await SkyHostBrowser.moveCursorOffScreen();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('text-editor-link-modal')
+      });
+    });
+
+    it('should match merge field screenshot', async (done) => {
+      await element(by.css(
+        '.sky-text-editor-menu-MergeField .sky-dropdown-button'
+      )).click();
+      await element(by.css('.sky-dropdown-item button')).click();
+      await SkyHostBrowser.moveCursorOffScreen();
+      setTimeout(() => {
+        expect('#screenshot-text-editor').toMatchBaselineScreenshot(done, {
+          screenshotName: getScreenshotName('text-editor-merge-field')
+        });
+      }, 1000);
+    });
+  }
+
+  describe('(size: lg)', () => {
+    beforeEach( async() => {
+      currentTheme = undefined;
+      currentThemeMode = undefined;
+      await SkyHostBrowser.get('visual/text-editor');
+      await setBrowserSize('lg');
+    });
+
+    runTests();
+
+    describe('when modern theme', () => {
+      beforeEach(async () => {
+        await selectTheme('modern', 'light');
+      });
+
+      runTests();
+    });
+
+    describe('when modern theme in dark mode', () => {
+      beforeEach(async () => {
+        await selectTheme('modern', 'dark');
+      });
+
+      runTests();
+    });
+  });
+
+  describe('(size: xs)', () => {
+    beforeEach( async() => {
+      currentTheme = undefined;
+      currentThemeMode = undefined;
+      await SkyHostBrowser.get('visual/text-editor');
+      await setBrowserSize('xs');
+    });
+
+    runTests();
+
+    describe('when modern theme', () => {
+      beforeEach(async () => {
+        await selectTheme('modern', 'light');
+      });
+
+      runTests();
+    });
+
+    describe('when modern theme in dark mode', () => {
+      beforeEach(async () => {
+        await selectTheme('modern', 'dark');
+      });
+
+      runTests();
+    });
+  });
+
+});
