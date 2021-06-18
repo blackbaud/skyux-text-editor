386cc6692275dc8232ab99d35da36ee85e2616d0
Alex Kingman <alex.kingman@blackbaud.me>
2021-06-17 22:47:02 -0400

    visual tests

--- e2e/text-editor.e2e-spec.ts
+++ e2e/text-editor.e2e-spec.ts
@@ -5,37 +5,90 @@ import {
 
 import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
 describe('Text editor', () => {
-  beforeEach(() => {
-    SkyHostBrowser.get('visual/text-editor');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-  });
+  let currentTheme: string;
+  let currentThemeMode: string;
 
-  it('should match basic screenshot', (done) => {
-    expect('form').toMatchBaselineScreenshot(done, {
-      screenshotName: 'text-editor-basic'
-    });
-  });
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
 
-  it('should match link modal screenshot', (done) => {
-    element(by.css('.Link button')).click();
-    SkyHostBrowser.moveCursorOffScreen();
-    expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'text-editor-link-modal'
-    });
-  });
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
 
-  it('should match merge field screenshot', (done) => {
-    element(by.css('.MergeField .sky-dropdown-button')).click();
-    element(by.css('.sky-dropdown-item button')).click();
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
 
-    setTimeout(() => {
-      expect('form').toMatchBaselineScreenshot(done, {
-        screenshotName: 'text-editor-merge-field'
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function runTests(): void {
+    it('should match screenshot', (done) => {
+      expect('#screenshot-text-editor').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('text-editor')
       });
-    }, 1000);
+    });
+
+    it('should match link modal screenshot', async (done) => {
+      await element.all(by.css('.sky-text-editor-toolbar-action-Link button')).first().click();
+      await SkyHostBrowser.moveCursorOffScreen();
+      expect('body').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('text-editor-link-modal')
+      });
+    });
+
+    it('should match merge field screenshot', async (done) => {
+      await element(by.css(
+        '.sky-text-editor-menubar-action-MergeField .sky-dropdown-button'
+      )).click();
+      await element(by.css('.sky-dropdown-item button')).click();
+      await SkyHostBrowser.moveCursorOffScreen();
+      setTimeout(() => {
+        expect('#screenshot-text-editor').toMatchBaselineScreenshot(done, {
+          screenshotName: getScreenshotName('text-editor-merge-field')
+        });
+      }, 1000);
+    });
+  }
+
+  beforeEach(async () => {
+    currentTheme = undefined;
+    currentThemeMode = undefined;
+
+    await SkyHostBrowser.get('visual/text-editor');
+    await SkyHostBrowser.setWindowBreakpoint('lg');
   });
+
+  runTests();
+
+  describe('when modern theme', () => {
+
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
+    });
+
+    runTests();
+
+  });
+
+  describe('when modern theme in dark mode', () => {
+
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
+    });
+
+    runTests();
+
+  });
+
 });
