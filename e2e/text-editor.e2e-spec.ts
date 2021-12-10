a7806adee984d70aa41b8b1c0cd8c8207d912230
Steve Brush <steve.brush@blackbaud.com>
2021-12-10 10:08:44 -0500

    Fix e2e for ADO (#34)

--- e2e/text-editor.e2e-spec.ts
+++ e2e/text-editor.e2e-spec.ts
@@ -9,8 +9,10 @@ import {
 } from '@skyux-sdk/e2e';
 
 import {
+  browser,
   by,
-  element
+  element,
+  ExpectedConditions
 } from 'protractor';
 
 describe('Text editor', () => {
@@ -76,7 +78,17 @@ describe('Text editor', () => {
       )).click();
       await element(by.css('.sky-dropdown-item button')).click();
       await SkyHostBrowser.moveCursorOffScreen();
-      expect('#screenshot-text-editor iframe').toMatchBaselineScreenshot(done, {
+
+      await browser.wait(
+        ExpectedConditions.elementToBeClickable(
+          element(by.css('#screenshot-text-editor .sky-text-editor-wrapper'))
+        ),
+        5000
+      );
+
+      await SkyHostBrowser.scrollTo('#screenshot-text-editor');
+
+      expect('#screenshot-text-editor .sky-text-editor-wrapper').toMatchBaselineScreenshot(done, {
         screenshotName: getScreenshotName('text-editor-merge-field')
       });
     });
